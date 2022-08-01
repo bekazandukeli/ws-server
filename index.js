f6716ae4115583bb1c44aa8e9a21de4a404ca2b0
Beka Zandukeli <beka.zandukeli@holovis.com>
2022-08-02 01:44:29 +0400

    added frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const WebSocket = require("ws");
-const express = require("express");
+const WebSocket = require('ws');
+const express = require('express');
+const open = require('open');
 
 const WS_PORT = 7071;
 const EXPRESS_PORT = 3001;
@@ -9,32 +10,31 @@ const app = express();
 
 const clients = new Map();
 
-wss.on("connection", (ws) => {
+wss.on('connection', (ws) => {
   clients.set(ws);
 
-  ws.on("close", () => {
+  ws.on('close', () => {
     clients.delete(ws);
   });
 });
 
-console.log("WSS is up on port:", WS_PORT);
+console.log('WSS is up on port:', WS_PORT);
 
+app.use(express.static('public'));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/ws", (req, res) => {
+app.post('/ws', (req, res) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(req.body));
     }
   });
-  console.log(JSON.stringify(req.body));
-  res.send("success");
+  console.log(req.body);
+  res.send('success');
 });
 
 app.listen(EXPRESS_PORT, () => {
   console.log(`Example app listening on port ${EXPRESS_PORT}`);
 });
+
+open('http://localhost:3001');
---
+++ public/scripts.js
@@ -0,0 +1,32 @@
+const textArea = document.getElementById('textarea');
+const button = document.getElementById('button');
+
+textArea.addEventListener('input', (e) => {
+  console.log(textArea.value);
+});
+
+textArea.addEventListener('keydown', function (e) {
+  if (e.key == 'Tab') {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value =
+      this.value.substring(0, start) + '\t' + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart = this.selectionEnd = start + 1;
+  }
+});
+
+button.addEventListener('click', (e) => {
+  fetch('/ws', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: textArea.value,
+  });
+});
