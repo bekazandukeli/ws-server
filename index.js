8e929e75b644f792cf765a0a12dcaed9096595d7
Beka Zandukeli <beka.zandukeli@holovis.com>
2022-08-01 12:29:02 +0400

    removed unused code

--- index.js
+++ index.js
@@ -1,62 +1,40 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 7071 });
+const WebSocket = require("ws");
+const express = require("express");
+
+const WS_PORT = 7071;
+const EXPRESS_PORT = 3001;
+
+const wss = new WebSocket.Server({ port: WS_PORT });
+const app = express();
 
 const clients = new Map();
 
-wss.on('connection', (ws) => {
-  const id = uuidv4();
-  const color = Math.floor(Math.random() * 360);
-  const metadata = { id, color };
+wss.on("connection", (ws) => {
+  clients.set(ws);
 
-  clients.set(ws, metadata);
-
-  ws.on('message', (messageAsString) => {
-
-    const message = JSON.parse(messageAsString);
-    const metadata = clients.get(ws);
-
-    message.sender = metadata.id;
-    message.color = metadata.color;
-
-    const outbound = JSON.stringify(message);
-
-    [...clients.keys()].forEach((client) => {
-      client.send(outbound);
-    });
-  });
   ws.on("close", () => {
     clients.delete(ws);
   });
 });
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-console.log("wss up");
+console.log("WSS is up on port:", WS_PORT);
 
-const express = require('express')
-const app = express()
-const port = 3001
+app.use(express.json());
 
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post('/ws', (req, res) => {
-  wss.clients.forEach(client => {
+app.post("/ws", (req, res) => {
+  wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(req.body));
     }
   });
   console.log(JSON.stringify(req.body));
-  res.send('success');
+  res.send("success");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Example app listening on port ${EXPRESS_PORT}`);
+});
